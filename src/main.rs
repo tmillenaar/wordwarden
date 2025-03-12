2881184b03b7fcafd75ca20f66703030a966df72
Timo Millenaar <timo.m@live.nl>
2025-03-12 22:41:07 +0100

    Iteratively check directories

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 use regex::{Captures, Regex};
 use std::env;
-use std::fs::File;
+use std::fs::{read_dir, File};
 use std::io::{self, BufRead};
 use std::path::Path;
+use std::path::PathBuf;
 use std::process;
 
 const BOLD_START: &str = "\x1b[1m";
@@ -24,6 +25,25 @@ fn highlight_text(line: &str, highlight_text: &str) -> String {
         format!("{}{}{}", BOLD_START, &captures[0], BOLD_END)
     })
     .to_string()
+}
+
+fn files_in_dir(dir: &Path) -> std::io::Result<Vec<PathBuf>> {
+    let mut files: Vec<PathBuf> = Vec::new();
+
+    for entry in read_dir(dir)? {
+        let entry = entry?;
+        let path = entry.path();
+        if path.is_file() {
+            files.push(path);
+        } else if path.is_dir() {
+            match files_in_dir(path.as_path()) {
+                Ok(entries) => files.extend(entries),
+                _ => {} // Don't check any files that cause errors when checking if they are a file
+            };
+        }
+    }
+
+    Ok(files)
 }
 
 fn check_file(
@@ -69,27 +89,32 @@ fn check_file(
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
+    if args.len() < 2 || args.contains(&"-h".to_string()) || args.contains(&"--help".to_string()) {
         eprintln!(
             "
-            Usage: {} <file1> <file2> 'word1' 'word2' 'word3']
+            Usage: {} <file1> <file2> 'word1' 'word2' 'word3'
             Use --casecheck and --no-casecheck to determine whether the check should be case-sensitive.
                 The default is to not be case sensitive.
             Use '--escape=skip-this-line' to ignore occurances of words found on a line with the specified escape string.
                 The default escape string is 'wordwarden:skip-line'
+            Use -w to force the interpretation of the argument as a word if it also happens to be the name of
+                a file or directory. This might look like: {} examples -w 'examples'
+                In that example we check for the word 'examples' in the files in the folder called examples
         ",
-            args[0]
+            args[0], args[0]
         );
         process::exit(2);
     }
 
-    let mut filepaths: Vec<&Path> = Vec::new();
+    let mut filepaths: Vec<PathBuf> = Vec::new();
     let mut search_strings: Vec<&String> = Vec::new();
     let mut check_case: bool = false;
     let mut escape: String = "wordwarden:skip-line".to_string();
 
-    for arg in &args[1..] {
-        let path = Path::new(arg);
+    let mut i = 1;
+    for _ in 1..(args[1..].len()) {
+        let arg = &args[i];
+        let path = Path::new(&arg);
         if path.is_file() {
             // For better integration with pre-commit, don't check the .pre-commit-config.yaml
             // for occurences because by the way the hook is set up, you specify the arguments
@@ -98,24 +123,40 @@ fn main() {
             if (path.file_name().unwrap() != ".pre-commit-config.yaml")
                 && (path.file_name().unwrap() != ".pre-commit-config.yml")
             {
-                filepaths.push(path)
+                filepaths.push(path.to_path_buf())
             }
+        } else if path.is_dir() {
+            match files_in_dir(path) {
+                Ok(entries) => filepaths.extend(entries),
+                _ => {} // Don't check any files that cause errors when checking if they are a file
+            };
         } else if arg.starts_with("--casecheck") {
             check_case = true;
         } else if arg.starts_with("--no-casecheck") {
             check_case = false;
         } else if arg.starts_with("--escape=") {
             escape = arg.replace("--escape=", "");
+        } else if arg.starts_with("-a") {
+            // Treat -w as the precursor for a word to check, append the next word to the search_strings vec
+            i += 1;
+            search_strings.push(&args[i])
         } else {
-            search_strings.push(arg);
+            search_strings.push(&arg);
         }
+        i += 1;
     }
 
     let mut found_any = false;
     let mut results: Vec<Occurance> = Vec::new();
     for path in filepaths {
         for target in &search_strings {
-            match check_file(path, &mut results, target, check_case, &escape) {
+            match check_file(
+                &path.to_path_buf(),
+                &mut results,
+                target,
+                check_case,
+                &escape,
+            ) {
                 Ok(found) => {
                     if found {
                         found_any = true;
